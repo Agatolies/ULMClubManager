837e2e8bf811b2928163fc2fd2c852eb35e0f41f
Agatolies <42890726+Agatolies@users.noreply.github.com>
2021-08-07 16:27:09 +0200

    Create IND repository and model

---
+++ src/ULMClubManager/ULMClubManager.DAL/Models/Ind.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ULMClubManager.DAL.Models
+{
+    public class Ind
+    {
+        public int IND_ID { get; set; }
+        public DateTime IND_DTE_DEB { get; set; }
+        public string IND_HEU_DEB { get; set; }
+        public DateTime IND_DTE_FIN { get; set; }
+        public string IND_HEU_FIN { get; set; }
+        public string IND_DES { get; set; }
+        public int AER_ID { get; set; }
+    }
+}
---
+++ src/ULMClubManager/ULMClubManager.DAL/Repositories/IndRepository.cs
@@ -0,0 +1,13 @@
+﻿using ULMClubManager.DAL.Abstractions;
+using ULMClubManager.DAL.Models;
+
+namespace ULMClubManager.DAL.Repositories
+{
+    public class IndRepository : GenericRepository<Ind, int>
+    {
+        public IndRepository(string connectionString)
+            : base(connectionString, "IND")
+        {
+        }
+    }
+}
