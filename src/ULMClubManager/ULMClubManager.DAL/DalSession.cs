8daa16da1c9c0da58cdd381bbeb9d3fff00a2e5b
Agatolies <42890726+Agatolies@users.noreply.github.com>
2021-08-09 14:36:53 +0200

    Update DalSession

--- src/ULMClubManager/ULMClubManager.DAL/DalSession.cs
+++ src/ULMClubManager/ULMClubManager.DAL/DalSession.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ULMClubManager.DAL.Abstractions;
 using ULMClubManager.DAL.Mappers;
 using ULMClubManager.DAL.Repositories;
@@ -15,8 +11,18 @@ namespace ULMClubManager.DAL
     {
         private IDbConnection _connection;
         private IUnitOfWork _unitOfWork;
-        private LocalityRepository _localities;
+
         private AircraftRepository _aircrafts;
+        private BookingRepository _bookings;
+        private CancellationRepository _cancellations;
+        private CategoryRepository _categories;
+        private LocalityRepository _localities;
+        private PilotRepository _pilots;
+        private RunwayRepository _runways;
+        private SubscriptionRepository _subscriptions;
+        private SupporterRepository _supporters;
+        private UnavailabilityRepository _unavailabilities;
+        private WithdrawalRepository _withdrawals;
 
         public DalSession()
         {
@@ -32,35 +38,115 @@ namespace ULMClubManager.DAL
             get { return _unitOfWork; }
         }
 
-        public LocalityRepository Localities 
-        { 
-            get
-            {
-                if (_localities == null)
-                {
-                    LocalityMapper localityMapper = new LocalityMapper();
-                    _localities = new LocalityRepository(_unitOfWork, localityMapper);
-                }
-
-                return _localities;
-            }
-        }
         public AircraftRepository Aircrafts 
         { 
             get
             {
-                if (_aircrafts == null)
-                {
-                    AircraftMapper aircraftMapper = new AircraftMapper();
-                    _aircrafts = new AircraftRepository(_unitOfWork, aircraftMapper);
-                }
+                return _aircrafts ?? (_aircrafts = new AircraftRepository(_unitOfWork, new AircraftMapper()));
 
-                return _aircrafts;
+                // La ligne au-dessus est identique au code ci-dessous
+                // if (_aircrafts == null)
+                // {
+                //     _aircrafts = new AircraftRepository(_unitOfWork, new AircraftMapper());
+                // }
+                // return _aircrafts;
+            }
+        }
+
+        public BookingRepository Bookings
+        {
+            get
+            {
+                return _bookings ?? (_bookings = new BookingRepository(_unitOfWork, new BookingMapper()));
+            }
+        }
+
+        public CancellationRepository Cancellations
+        {
+            get
+            {
+                return _cancellations ?? (_cancellations = new CancellationRepository(_unitOfWork, new CancellationMapper()));
+            }
+        }
+
+        public CategoryRepository Categories
+        {
+            get
+            {
+                return _categories ?? (_categories = new CategoryRepository(_unitOfWork, new CategoryMapper()));
+            }
+        }
+
+        public LocalityRepository Localities
+        {
+            get
+            {
+                return _localities ?? (_localities = new LocalityRepository(_unitOfWork, new LocalityMapper()));
+            }
+        }
+
+        public PilotRepository Pilots
+        {
+            get
+            {
+                return _pilots ?? (_pilots = new PilotRepository(_unitOfWork, new PilotMapper()));
+            }
+        }
+
+        public RunwayRepository Runways
+        {
+            get
+            {
+                return _runways ?? (_runways = new RunwayRepository(_unitOfWork, new RunwayMapper()));
+            }
+        }
+
+        public SubscriptionRepository Subscriptions
+        {
+            get
+            {
+                return _subscriptions ?? (_subscriptions = new SubscriptionRepository(_unitOfWork, new SubscriptionMapper()));
+            }
+        }
+
+        public SupporterRepository Supporters
+        {
+            get
+            {
+                return _supporters ?? (_supporters = new SupporterRepository(_unitOfWork, new SupporterMapper()));
+            }
+        }
+
+        public UnavailabilityRepository Unavailabilities
+        {
+            get
+            {
+                return _unavailabilities ?? (_unavailabilities = new UnavailabilityRepository(_unitOfWork, new UnavailabilityMapper()));
+            }
+        }
+
+        public WithdrawalRepository Withdrawals
+        {
+            get
+            {
+                return _withdrawals ?? (_withdrawals = new WithdrawalRepository(_unitOfWork, new WithdrawalMapper()));
             }
         }
 
         public void Dispose()
         {
+            _aircrafts = null;
+            _bookings = null;
+            _cancellations = null;
+            _categories = null;
+            _localities = null;
+            _pilots = null;
+            _runways = null;
+            _subscriptions = null;
+            _supporters = null;
+            _unavailabilities = null;
+            _withdrawals = null;
+
             _unitOfWork.Dispose();
             _connection.Dispose();
         }
--- src/ULMClubManager/ULMClubManager.DapperIntegration/Program.cs
+++ src/ULMClubManager/ULMClubManager.DapperIntegration/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using ULMClubManager.DAL;
-using ULMClubManager.DAL.Abstractions;
-using ULMClubManager.DAL.DomainModels;
+using ULMClubManager.BL.Services;
 
 namespace ULMClubManager.DapperIntegration
 {
@@ -10,84 +7,17 @@ namespace ULMClubManager.DapperIntegration
     {
         static void Main(string[] args)
         {
-            ReadAll();
-            //ReadOne();
-            //CreateOne();
-            //UpdateOne();
-            //DeleteOne();
+            LocalityService localityService = new LocalityService();
+
+            localityService.ReadAll();
+            //localityService.ReadOne();
+            //localityService.CreateOne();
+            //localityService.UpdateOne();
+            //localityService.DeleteOne();
 
             Console.WriteLine("ok");
 
             Console.ReadLine();
-        }
-
-        private static void DeleteOne()
-        {
-            using (DalSession dalSession = new DalSession())
-            {
-                dalSession.Localities.DeleteOne(8);
-            }
-        }
-
-        private static void UpdateOne()
-        {
-            using (DalSession dalSession = new DalSession())
-            {
-                Locality loc = dalSession.Localities.ReadOne(8);
-                loc.Name = "Longdoz";
-                dalSession.Localities.UpdateOne(loc);
-            }
-        }
-
-        private static void CreateOne()
-        {
-            using(DalSession dalSession = new DalSession())
-            {
-                Locality newLoc = new Locality("4020", "Longdoz");
-                dalSession.Localities.CreateOne(newLoc);
-            }
-        }
-
-        private static void CreateOneWithTransaction()
-        {
-            using (DalSession dalSession = new DalSession())
-            {
-                IUnitOfWork unitOfWork = dalSession.UnitOfWork;
-                unitOfWork.Begin();
-                try
-                {
-                    Locality newLoc = new Locality("4020", "Longdoz");
-                    dalSession.Localities.CreateOne(newLoc);
-                    unitOfWork.Commit();
-                }
-                catch
-                {
-                    unitOfWork.Rollback();
-                    throw;
-                }
-            }
-        }
-
-        private static void ReadAll()
-        {
-            using (DalSession dalSession = new DalSession())
-            {
-                IEnumerable<Locality> localites = dalSession.Localities.ReadAll();
-
-                foreach (var item in localites)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-        }
-
-        private static void ReadOne()
-        {
-            using (DalSession dalSession = new DalSession())
-            {
-                Locality localite = dalSession.Localities.ReadOne(3);
-                Console.WriteLine(localite);
-            }
         }
     }
 }
